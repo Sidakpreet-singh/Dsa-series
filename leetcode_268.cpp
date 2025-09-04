97b772a06911dc369785737b41994a16be842b5c
Sidakpreet-singh <sidakpreetsingh013@gmail.com>
2025-09-04 23:50:16 +0530

    revise

---
+++ leetcode_268.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int>& nums) {
+        unordered_set <int> s(nums.begin(),nums.end());
+        int ans = -1;
+        int n = nums.size();
+
+        for(int i = 0; i<=n;i++){
+            if(s.find(i) == s.end()){
+                ans = i;
+            }
+        }
+
+        return ans;
+        
+    }
+
+int main(){
+  vector <int> arr = {0,1,3};
+  cout<<missingNumber(arr);
+}
